6afe1a6e29b96500e4fc895d82304f53dfc6c8e5
ginibeagha <ginibeagha@gmail.com>
2016-06-06 06:07:03 +0200

    testtry not fully working

--- myPythonscripts.py
+++ myPythonscripts.py
@@ -1,6 +1,51 @@
 def readfile(str):
-    with open(str, mode='r', encoding='cp1252', errors='ignore') as datafile:
+    with open(str, mode='r', errors='ignore') as datafile:
         for number, line in enumerate(datafile.readlines()):
             print(number,': ',line)
         
-readfile('C:\\Users\\Ginibeagha\\Downloads\\pythontest.txt')
+#readfile('C:\\Users\\Ginibeagha\\Downloads\\pythontest.txt')
+
+def writeFile(string):
+    with open("newfile.txt", 'w') as temp:
+        print(string, file=temp)
+        
+#writeFile('this is just a taste of writing a new file')
+
+def testMethod():
+    while True:
+        command= input("please choose one of the options [rwq]: ")
+        print(command)
+        if command=='q':
+            break
+        elif command.lower()=='r':
+            print('you have chosen to move right')
+            continue
+        elif command=='w':
+            print('you have chosen the w option')
+            continue
+        else:
+            print('please one of [rwq]')
+            continue
+
+#testMethod()
+
+def testtry():
+    while True:
+        try:
+            value= input('choose a number between 1 and 20')
+            print(int(value))
+            break
+        except ValueError:
+            print ('that was not a number')
+            continue
+        if int(value) > 20:
+            print('number is too high')
+            continue
+        elif int(value) < 1:    
+            print ('number too low')
+            continue
+        elif value=='q':
+            break
+            
+
+testtry()        
