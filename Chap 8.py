d40d8833cf24ba1d7b46159e77e8e0059dd94014
ginibeagha <ginibeagha@gmail.com>
2016-07-28 23:29:44 +0200

    day2 Chap8

--- Chap 8.py
+++ Chap 8.py
@@ -9,3 +9,8 @@ print(os.getcwd())
 os.chdir('.\\newfolder')
 print(os.getcwd())
 print(os.path.abspath('..\..'))
+print(os.path.getsize(os.path.join('d:','\\')))
+for file in os.listdir(os.path.join('\Program Files (x86)','putty')):
+    print(file +" size is "+ str(os.path.getsize(os.path.join('\Program Files (x86)\putty', file))/1000)+" kb")
+
+
